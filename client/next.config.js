204a5344225f1e1f3fc010f1b9044d834c80bd09
Yemi Oduyemi <yhermii@gmail.com>
2023-09-21 03:02:22 +0100

    Updates on next.config file

--- client/next.config.js
+++ client/next.config.js
@@ -2,34 +2,26 @@
  * @type {import('next').NextConfig}
  */
 
- const webpack = require("webpack");
+ module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      })
+    );
 
+    return config;
+  },
+  images: {
+    domains: [process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || ""], 
+  },
+  env: {
+    CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  },
+ };
+ 
 
-module.exports = (phase, { defaultConfig }) => {
-  const nextConfig = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery",
-          "window.jQuery": "jquery",
-        })
-      );
-
-      return config;
-    },
-    images: {
-      domains: [process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME],
-    },
-    env: {
-      CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-    },
-  };
-
-  return {
-    ...defaultConfig,
-    ...nextConfig,
-  };
-};
