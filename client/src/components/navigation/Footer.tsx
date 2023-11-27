e38ebfbd79c1efbbc93030eb1ef14f48b18b706d
Yemi Oduyemi <yhermii@gmail.com>
2023-11-27 19:07:09 +0100

    5

--- client/src/components/navigation/Footer.tsx
+++ client/src/components/navigation/Footer.tsx
@@ -52,7 +52,7 @@ export const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div className="footer_social_area mb-8 mt-4 md:mb-0">
+            <div className="single_widget_area mb-8 mt-4 md:mb-0">
                     <Link href="/">
                         <Image
                             src="/assets/images/svg/logo-facebook.svg"
