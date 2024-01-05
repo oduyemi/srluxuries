38905133969cf5d73788d63685612557ceefc451
Yemi Oduyemi <yhermii@gmail.com>
2024-01-06 00:44:13 +0100

    edit 1

--- client/src/components/navigation/Footer.tsx
+++ client/src/components/navigation/Footer.tsx
@@ -12,10 +12,16 @@ export const Footer = () => {
                 Find us on any of these platforms, we respond fast.
                 </h5>
                 <div className="mt-6 lg:mb-0 mb-6">
-                <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    <i className="fab fa-twitter"></i></button><button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    <i className="fab fa-facebook-square"></i></button><button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    <i className="fab fa-dribbble"></i></button><button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <Link href="" className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2">
+                        <i className="fab fa-twitter"></i>
+                    </Link>
+                    <Link href="" className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2">
+                        <i className="fab fa-facebook-square"></i>
+                    </Link>
+                    <Link href="" className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2">
+                        <i className="fab fa-mail"></i>
+                    </Link>
+                    <button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
                     <i className="fab fa-github"></i>
                 </button>
                 </div>
@@ -23,19 +29,19 @@ export const Footer = () => {
             <div className="w-full lg:w-6/12 px-4">
                 <div className="flex flex-wrap items-top mb-6">
                 <div className="w-full lg:w-4/12 px-4 ml-auto">
-                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
+                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Navigation Links</span>
                     <ul className="list-unstyled">
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/about">About Us</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/accesories">Accesories</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/collections">Collections</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/privacy-policy">Privacy Policy</Link>
                     </li>
                     </ul>
                 </div>
@@ -43,16 +49,16 @@ export const Footer = () => {
                     <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
                     <ul className="list-unstyled">
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/services">Services</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/appointment">Book Appointment</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/privacy-policy">Privacy Policy</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</Link>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/locator">Contact Us</Link>
                     </li>
                     </ul>
                 </div>
@@ -63,9 +69,7 @@ export const Footer = () => {
             <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
                 <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © <span id="get-current-year">2021</span>
-                <Link href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by
-                <Link href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim</Link>.
+                Copyright &copy; Samuel Rowland Luxuries | All rights reserved.
                 </div>
             </div>
             </div>
