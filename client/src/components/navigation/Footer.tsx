71779450750b86978e17773dbd61cf16ea94c25b
Yemi Oduyemi <yhermii@gmail.com>
2024-01-06 00:18:31 +0100

    footer redo

--- client/src/components/navigation/Footer.tsx
+++ client/src/components/navigation/Footer.tsx
@@ -3,96 +3,73 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="footer_area">
-    <div className="container">
-        <div className="flex flex-wrap">
-            <div className="w-full md:w-1/2">
-                <div className="single_widget_area flex mb-8 md:mb-0">
-                    <div className="footer-logo mr-4 md:mr-8">
-                        <Link href="/">
-                            <Image
-                                src="/assets/images/logo-white.png"
-                                alt="Logo"
-                                width={50}
-                                height={50}
-                                className="single-brands-logo pl-8"
-                            />
-                        </Link>
-                    </div>
-                    <div className="footer_menu mb-5">
-                        <ul className="pl-4">
-                            <li>
-                                <Link href="/">Shop</Link>
-                                <ul>
-                                    <li><p className="text-white text-sm w-1/2">117, Obafemi Awolowo Way, Ikeja, Lagos</p></li>
-                                </ul>
-                            </li>
-                            {/* <li>
-                                <Link href="/services">Services</Link>
-                                <ul>
-                                    <li><p className="text-white text-sm">Custom Designs</p></li>
-                                </ul>
-                            </li> */}
-                            <li>
-                                <Link href="/locator">Contact</Link>
-                                <ul>
-                                    <li><p className="text-white text-sm w-2/3">support@thesrluxuries.com</p></li>
-                                    {/* <li><p className="text-white text-sm w-2/3">info@thesrluxuries.com</p></li> */}
-                                    {/* <li><p className="text-white text-sm">08188777223</p></li> */}
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
+    <footer className="relative bg-brownie pt-8 pb-6">
+        <div className="container mx-auto px-4">
+            <div className="flex flex-wrap text-left lg:text-left">
+            <div className="w-full lg:w-6/12 px-4">
+                <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
+                <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
+                Find us on any of these platforms, we respond fast.
+                </h5>
+                <div className="mt-6 lg:mb-0 mb-6">
+                <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i className="fab fa-twitter"></i></button><button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i className="fab fa-facebook-square"></i></button><button className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i className="fab fa-dribbble"></i></button><button className="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+                    <i className="fab fa-github"></i>
+                </button>
                 </div>
             </div>
-            {/* Single Widget Area */}
-                <div className="single_widget_area mb-8 mt-4 md:mb-0">
-                    <ul className="footer_widget_menu">
-                        <li><Link href="/privacy-policy">Privacy Policy</Link></li>
+            <div className="w-full lg:w-6/12 px-4">
+                <div className="flex flex-wrap items-top mb-6">
+                <div className="w-full lg:w-4/12 px-4 ml-auto">
+                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
+                    <ul className="list-unstyled">
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</Link>
+                    </li>
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</Link>
+                    </li>
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</Link>
+                    </li>
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</Link>
+                    </li>
                     </ul>
                 </div>
-            </div>
-            <div className="w-full md:w-1/2">
-                <div className="single_widget_area">
-                    <div className="footer_social_area">
-                        <Link href="https://www.facebook.com/sr_luxuries" target="_blank" data-toggle="tooltip" data-placement="top" title="Facebook">
-                            <Image
-                                src="/assets/images/svg/logo-facebook.svg"
-                                alt="Facebook"
-                                width={20}
-                                height={20}
-                                className="footer_icons w-7"
-                            />
-                        </Link>
-                        <Link href="https://www.instagram.com/sr_luxuries/" target="_blank" data-toggle="tooltip" data-placement="top" title="Instagram">
-                            <Image
-                                src="/assets/images/svg/logo-instagram.svg"
-                                alt="Instagram"
-                                width={20}
-                                height={20}
-                                className="footer_icons w-7"
-                            />
-                        </Link>
-                        <Link href="https://www.twitter.com/srluxuries" target="_blank" data-toggle="tooltip" data-placement="top" title="Twitter">
-                            <Image
-                                src="/assets/images/svg/logo-twitter.svg"
-                                alt="Twitter"
-                                width={40}
-                                height={40}
-                                className="footer_icons w-7"
-                            />
-                        </Link>
-                    </div>
+                <div className="w-full lg:w-4/12 px-4">
+                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
+                    <ul className="list-unstyled">
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</Link>
+                    </li>
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</Link>
+                    </li>
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</Link>
+                    </li>
+                    <li>
+                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</Link>
+                    </li>
+                    </ul>
                 </div>
-            </div>   
-        <div className="mt-5">
-            <div className="w-full text-center text-butter">
-                <p>
-                    Copyright &copy; Samuel Rowland Luxuries | All rights reserved.
-                </p>
+                </div>
+            </div>
+            </div>
+            <hr className="my-6 border-blueGray-300" />
+            <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+                <div className="text-sm text-blueGray-500 font-semibold py-1">
+                Copyright © <span id="get-current-year">2021</span>
+                <Link href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by
+                <Link href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim</Link>.
+                </div>
+            </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
   );
 };
