35758e343a53fec8453f33a20ea520cdee73ae32
Yemi Oduyemi <yhermii@gmail.com>
2024-01-06 05:28:13 +0100

    popup style

--- client/src/components/navigation/Popup.tsx
+++ client/src/components/navigation/Popup.tsx
@@ -1,3 +1,4 @@
+// Popup.tsx
 import React from "react";
 import { Box } from "@mui/material";
 
@@ -11,11 +12,36 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
     <Box
       className={`popup ${isOpen ? "open" : ""}`}
       onClick={onClose}
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        display: "block",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        visibility: isOpen ? "visible" : "hidden",
+        opacity: isOpen ? 1 : 0,
+        transition: "visibility 0s, opacity 0.5s",
+      }}
     >
-      <h2>Email Addresses</h2>
-      <p>info@thesrluxuries.com</p>
-      <p>support@thesrluxuries.com</p>
-      <button onClick={onClose}> X </button>
+      <Box
+        className="popup-content"
+        onClick={(e:any) => e.stopPropagation()}
+        sx={{
+          backgroundColor: "white",
+          padding: "20px",
+          borderRadius: "8px",
+          textAlign: "center",
+        }}
+      >
+        <h2>Email Addresses</h2>
+        <p>info@thesrluxuries.com</p>
+        <p>support@thesrluxuries.com</p>
+        <button onClick={onClose}> X </button>
+      </Box>
     </Box>
   );
 };
