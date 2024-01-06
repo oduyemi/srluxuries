d3b563f0f1d5f306ee6002d21e35f8ce5716efdb
Yemi Oduyemi <yhermii@gmail.com>
2024-01-06 01:09:51 +0100

    edit4

--- client/src/components/navigation/Footer.tsx
+++ client/src/components/navigation/Footer.tsx
@@ -1,20 +1,22 @@
+import { Box } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 export const Footer = () => {
   return (
     <footer className="relative bg-tan pt-8 pb-6">
-        <div className="container mx-auto px-4">
-            <div className="flex flex-wrap text-left lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
+        <Box className="container mx-auto px-4">
+            <Box className="flex flex-wrap text-left lg:text-left">
+            <Box className="w-full lg:w-6/12 px-4">
                 <h4 className="text-3xl fonat-semibold text-butter">Let&apos;s keep in touch!</h4>
                 <h5 className="text-lg mt-0 mb-2 text-white">
                 Find us on any of these platforms, we respond fast.
                 </h5>
-                <div className="mt-6 lg:mb-0 mb-6">
+                <Box className="mt-6 lg:mb-0 mb-6">
                     <Link href="https://www.twitter.com/srluxuries">
                         <button className="bg-white text-bluey shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2">
-                            <i className="fab fa-twitter"></i>
+                            {TwitterIcon}
                     </button>
                     </Link>
                     {/* <Link href="" className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2">
@@ -28,56 +30,56 @@ export const Footer = () => {
                     <button className="bg-white text-butter shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
                     <i className="fab fa-github"></i>
                 </button>
-                </div>
-            </div>
-            <div className="w-full lg:w-6/12 px-4">
-                <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 ml-auto">
-                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Navigation Links</span>
+                </Box>
+            </Box>
+            <Box className="w-full lg:w-6/12 px-4">
+                <Box className="flex flex-wrap items-top mb-6">
+                <Box className="w-full lg:w-4/12 px-4 ml-auto">
+                    <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
                     <ul className="list-unstyled">
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/about">About Us</Link>
+                        <Link className="text-blueGray-600 hover:text-butter font-semibold block pb-2 text-sm" href="/about">About Us</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/accesories">Accesories</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/accesories">Accesories</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/collections">Collections</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/collections">Collections</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/privacy-policy">Privacy Policy</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/privacy-policy">Privacy Policy</Link>
                     </li>
                     </ul>
-                </div>
-                <div className="w-full lg:w-4/12 px-4">
+                </Box>
+                <Box className="w-full lg:w-4/12 px-4">
                     <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
                     <ul className="list-unstyled">
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/services">Services</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/services">Services</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/appointment">Book Appointment</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/appointment">Book Appointment</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/privacy-policy">Privacy Policy</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/privacy-policy">Privacy Policy</Link>
                     </li>
                     <li>
-                        <Link className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="/locator">Contact Us</Link>
+                        <Link className="text-blueGray-600 hover:text-white font-semibold block pb-2 text-sm" href="/locator">Contact Us</Link>
                     </li>
                     </ul>
-                </div>
-                </div>
-            </div>
-            </div>
-            <hr className="my-6 border-blueGray-300" />
-            <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-                <div className="text-sm text-blueGray-500 font-semibold py-1">
+                </Box>
+                </Box>
+            </Box>
+            </Box>
+            <hr className="my-6 border-goldie" />
+            <Box className="flex flex-wrap items-center md:justify-between justify-center">
+            <Box className="w-full md:w-4/12 px-4 mx-auto text-center">
+                <Box className="text-sm text-subtext font-semibold py-1">
                 Copyright &copy; Samuel Rowland Luxuries | All rights reserved.
-                </div>
-            </div>
-            </div>
-        </div>
+                </Box>
+            </Box>
+            </Box>
+        </Box>
     </footer>
   );
 };
